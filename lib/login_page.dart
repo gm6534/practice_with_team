b076fb181d6a238173c072a15d6400b065807339
ghulam.mustafa <ghulam.mustafa@autosmarttech.com>
2023-09-25 00:45:30 +0500

    push to azeem

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -27,52 +27,58 @@ class _LoginPageState extends State<LoginPage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.purpleAccent,
-        title: const Text("Login Page", style: TextStyle(color: Colors.white),),
+        title: const Text(
+          "Login Page",
+          style: TextStyle(color: Colors.white),
+        ),
         centerTitle: true,
       ),
-      body:  Padding(
+      body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 15.0),
         child: Form(
           key: _formKey,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              const SizedBox(height:20),
+              const SizedBox(height: 20),
               Center(
                 child: Container(
                   decoration: BoxDecoration(
-                    border: Border.all(width: 5, color: Colors.yellow)
-                  ),
+                      border: Border.all(width: 5, color: Colors.yellow)),
                   child: const Padding(
                     padding: EdgeInsets.all(8.0),
-                    child: Text("Logo", style: TextStyle(
-                        color: Colors.purpleAccent,
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold),),
+                    child: Text(
+                      "Logo",
+                      style: TextStyle(
+                          color: Colors.purpleAccent,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold),
+                    ),
                   ),
                 ),
               ),
-              const SizedBox(height:20),
+              const SizedBox(height: 20),
               TextFormField(
                 controller: _userC,
                 validator: (value) {
-                  if(value == null || value.isEmpty) {
+                  if (value == null || value.isEmpty) {
                     return "Required*";
                   } else {
                     return null;
                   }
                 },
                 decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  label: Text("User Name"),
-                  hintText: "Enter User Name"
-                ),
+                    border: OutlineInputBorder(),
+                    label: Text("User Name"),
+                    hintText: "Enter User Name"),
               ),
-              const SizedBox(height: 20,),
+              const SizedBox(
+                height: 20,
+              ),
               TextFormField(
                 controller: _passC,
                 validator: (value) {
-                  if(value == null || value.isEmpty) {
+                  if (value == null || value.isEmpty) {
                     return "Required*";
                   } else {
                     return null;
@@ -81,50 +87,53 @@ class _LoginPageState extends State<LoginPage> {
                 decoration: const InputDecoration(
                     border: OutlineInputBorder(),
                     label: Text("Password"),
-                    hintText: "Enter Password"
-                ),
+                    hintText: "Enter Password"),
                 inputFormatters: [
                   LengthLimitingTextInputFormatter(12),
                   FilteringTextInputFormatter.allow(RegExp("[0-9a-zA-Z\$@%]")),
                 ],
               ),
-              const SizedBox(height: 40,),
+              const SizedBox(
+                height: 40,
+              ),
               SizedBox(
-                width: 300,
+                  width: 300,
                   child: CustomButton(
                     isLoading: isLoad,
                     isSuccess: isDone,
-                    btnElevation: 15,
+                    btnElevation: isLoad ? 0 : 15,
                     title: 'Login',
                     // btnShape: BoxShape.circle,
-                    onTap: () {
-                      print("=========onTap Custom Button========");
-                      isLoad = true;
-                      Future.delayed(const Duration(seconds: 3), (){
-                        setState(() {
-                          isLoad = false;
-
-                        });
-                      }).whenComplete(() => isDone = true)
-                          .whenComplete(() =>
-                          Future.delayed(const Duration(seconds: 2), () {
-                        setState(() {
-                          isDone = false;
-                        });
-                      }));
-                      setState(() { });
-                    },
-                    btnColor: isDone == true ? Colors.green : isLoad ? Colors.red : null,
-                    btnGradColor: (isLoad || isDone) == false ? const LinearGradient(
-                        begin: Alignment.topLeft,
-                        end: Alignment.bottomRight,
-                        colors: [
-                          Colors.deepPurple,
-                          Colors.deepOrange
-                        ]
-                    ) : null,
-                  )
-              )
+                    onTap: (isLoad || isDone)
+                        ? () {}
+                        : () {
+                            print("=========onTap Custom Button========");
+                            isLoad = true;
+                            Future.delayed(const Duration(seconds: 3), () {
+                              setState(() {
+                                isLoad = false;
+                              });
+                            }).whenComplete(() => isDone = true).whenComplete(
+                                () => Future.delayed(const Duration(seconds: 2),
+                                        () {
+                                      setState(() {
+                                        isDone = false;
+                                      });
+                                    }));
+                            setState(() {});
+                          },
+                    btnColor: isDone == true
+                        ? Colors.green
+                        : isLoad
+                            ? Colors.grey.shade400
+                            : null,
+                    btnGradColor: (isLoad || isDone) == false
+                        ? const LinearGradient(
+                            begin: Alignment.topLeft,
+                            end: Alignment.bottomRight,
+                            colors: [Colors.deepPurple, Colors.deepOrange])
+                        : null,
+                  ))
               // CupertinoButton.filled(
               //     onPressed: () {
               //       if(_formKey.currentState!.validate()){
